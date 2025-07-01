0e1dd0c563e8a6fba975df94def1f820ccd03643
Felipe Bosi <felipe.bosi@withclutch.com>
2025-07-01 16:33:20 -0300

    Add newsletter validation

--- src/screens/circulo_vip/components/newsletter.rs
+++ src/screens/circulo_vip/components/newsletter.rs
@@ -1,12 +1,46 @@
-use leptos::prelude::*;
+use leptos::{ev::{Event, SubmitEvent}, prelude::*};
+use validator::Validate;
+use serde::Deserialize;
 
 use crate::components::{button::Button, email::Email, text::{ElementType, Text, TextSize, TextWeight}};
+
+#[derive(Debug, Validate, Deserialize)]
+pub struct FormData {
+    #[validate(email(message = "Digite um e-mail válido"))]
+    pub email: String,
+}
+#[derive(Debug,Clone)]
+struct FormErrors {
+    email: Option<String>,
+}
+
 
 #[component]
 pub fn Newsletter(
     #[prop(into, optional)] class: Option<String>,
 ) -> impl IntoView {
     let (email, set_email) = signal("".to_string());
+    let (errors, set_errors) = signal(FormErrors {
+        email: None,
+    });
+    let on_submit = move | e: SubmitEvent| {
+        e.prevent_default();
+        let form_data = FormData {
+            email: email.get(),
+        };
+        form_data.validate().map_err(|e| {
+            let mut temp_errors = FormErrors {
+                email: None,
+            };
+            for (field, error) in e.field_errors() {
+                match field.as_ref() {
+                    "email" => temp_errors.email = Some(error.get(0).map_or("".to_string(), |e| e.to_string())),
+                    _ => {}
+                }
+            }
+            set_errors.set(temp_errors);
+        }).ok();
+    };
     view! {
         <div class={class.unwrap_or_default()}>
             <Text size={TextSize::H1} r#as={ElementType::H2} weight={TextWeight::Bold} class="text-white leading-none text-center pb-5">
@@ -15,12 +49,17 @@ pub fn Newsletter(
             <Text class="text-white leading-none text-center pb-10">
                 "Vamos ficar pertinho? Compartilho por email o processo de escrita, recomendações literárias, material extra do livro e fotos do Chicó."
             </Text>
-            <div class="flex items-center">
+            <form on:submit=on_submit class="flex items-center">
                 <Email placeholder="Email" name="email".into() value=email set_value=set_email class="flex-auto w-10" />
-                <Button class="rounded-none rounded-e-full flex-none">
+                <Button r#type="submit" class="rounded-none rounded-e-full flex-none">
                     "Enviar"
                 </Button>
-            </div>
+            </form>
+            <Show when=move || errors.read().email.is_some()>
+                <Text class="text-err ml-4 mt-3" size=TextSize::Small>
+                    {move || errors.read().email.clone().unwrap_or("".to_string())}
+                </Text>
+            </Show>
         </div>
     }
 }
